9a933a18ad8f2aab6675bb474d4074b4ca399cde
andreybelushenko <dfreeman@ukr.net>
2020-03-12 20:11:26 +0300

    методы add и поиск

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,42 +1,20 @@
 package main;
 
 public class Main {
-    public static void main(String[] args) {
-        Car myCar = new Car();
-
-        int res = sum(150, 15);
-        System.out.println(myCar.color);
-    }
-
-    public static int sum(int a, int b) {
-        a = a * 2;
-        return a + b;
-    }
-
-}
-
-
-class Car {
-    String color = "Super Car";
-    int weight;
-    int power;
-    int currentSpeed;
-
-    void gas(int speed) {
-        currentSpeed = currentSpeed + speed;
-    }
-
-    void stop() {
-        currentSpeed--;
-    }
-
-    void printcurrentSpeed() {
-        System.out.println(currentSpeed);
-    }
-
-    int getPower() {
-        return power + 100;
+     public static void main(String[] args) {
+        Tree BinaryTree = new Tree();
+        BinaryTree.add(8);
+        BinaryTree.add(3);
+        BinaryTree.add(1);
+        BinaryTree.add(6);
+        BinaryTree.add(4);
+        BinaryTree.add(7);
+        BinaryTree.add(10);
+         BinaryTree.add(9);
+         BinaryTree.add(10);
+        System.out.println(BinaryTree);
+         BinaryTree.containsNode(8);
+         BinaryTree.containsNode(11);
+         BinaryTree.containsNode(4);
     }
 }
-
-
---
+++ src/main/java/main/Tree.java
@@ -0,0 +1,108 @@
+package main;
+
+
+public class Tree {
+    Node root;
+    public void add(int value) {
+        root = addRecursive(root, value);
+
+    }
+
+    private Node addRecursive(Node current, int value) {
+        if (current == null) {
+            System.out.println(value + "Addded to the tree");
+            return new Node(value);
+        }
+        if (value == current.value) {
+            System.out.println(value + " value is already exists");
+            return current;    //value already exists
+        }
+        else if  (value < current.value) {
+            current.left = addRecursive(current.left, value);
+        }
+        else  {    // (value > current.value)
+            current.right = addRecursive(current.right, value);
+        }
+
+        return current;
+    }
+
+
+    public boolean containsNode(int value) {
+         boolean containsNodeFlag;
+          containsNodeFlag= containsNodeRecursive(root, value);
+          if (containsNodeFlag == true) {
+              System.out.println("Tree contains " + value);
+          }
+          else {
+              System.out.println("Tree NOT contains " + value);
+          }
+        return containsNodeFlag;
+    }
+
+    private boolean containsNodeRecursive(Node current, int value) {
+                ///
+        if (current == null) {
+            return false;
+        }
+        if (value == current.value) {
+            return true;    //value already exists
+        }
+        else if  (value < current.value) {
+            return  containsNodeRecursive(current.left, value);
+
+        }
+        else  {    // (value > current.value)
+            return containsNodeRecursive(current.right, value);
+        }
+    }
+
+    //delete
+    public void delete(int value) {
+        root = deleteRecursive(root, value);
+    }
+
+
+    private Node deleteRecursive(Node current, int value) {
+        if (current == null) {
+            return null;
+        }
+
+        if (value == current.value) {
+
+            // У Ноды нет потомков, случай 1
+            if (current.left == null && current.right == null) {
+                return null;
+            }
+
+            //У Ноды есть один (правый или левый потомок, случай 2)
+            if (current.right == null) {
+                return current.left;
+            }
+
+            if (current.left == null) {
+                return current.right;
+            }
+
+        }
+        if (value < current.value) {
+            current.left = deleteRecursive(current.left, value);
+            return current;
+        }
+        current.right = deleteRecursive(current.right, value);
+        return current;
+    }
+
+
+
+
+
+
+
+    @Override
+    public String toString() {
+        return "Tree{" +
+                "root=" + root +
+                '}';
+    }
+}
